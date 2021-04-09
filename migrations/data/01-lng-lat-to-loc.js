194b08a247bf97f1e2979bdbc25dcbacf5cea184
PiotrFe <38947052+PiotrFe@users.noreply.github.com>
2021-04-09 20:44:39 +0200

    first commit

---
+++ migrations/data/01-lng-lat-to-loc.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const pg = require("pg");
+
+const pool = new pg.Pool({
+  host: "localhost",
+  port: 5432,
+  database: "socialnetwork",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
+pool
+  .query(
+    `
+    UPDATE posts
+    SET loc = POINT(lng, lat)
+    WHERE loc IS NULL;
+`
+  )
+  .then(() => {
+    console.log("Update completed");
+    pool.end();
+  })
+  .catch((err) => console.error(err.message));
